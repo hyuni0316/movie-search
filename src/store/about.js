853949a66e6bcf96b65aae47e5c0ca965244cf98
hyuni0316 <hyunini0408@gmail.com>
2024-07-31 20:33:53 +0900

    Create Porject

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from '../core/heropy'
+
+export default new Store({
+  photo: 'https://avatars.githubusercontent.com/u/79395147?s=96&v=4',
+  name: 'Lina / Hyun Lee',
+  email: 'hyunini0408@gmail.com',
+  blog: 'https://heropy.blog',
+  github: 'https://github.com/hyuni0316',
+  repository: 'https://github.com/ParkYoungWoong/vanillajs-movie-app'
+})
